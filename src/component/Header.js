417789747b5fc0c84b390c09bc0119a525e81854
TanyaJain <jaintanya231@gmail.com>
2022-09-02 14:08:00 +0530

    first commit

---
+++ src/component/Header.js
@@ -0,0 +1,9 @@
+const Header=(props)=>{
+    return(
+        <header>
+            <h1> Hello {props.title}</h1>
+            <h1> Hi {props.title1}</h1>
+        </header>
+    );
+};
+export default Header;
