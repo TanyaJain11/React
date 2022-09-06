87428d9845cd86b7a8d2c86fdc6dc90328ad1805
TanyaJain <jaintanya231@gmail.com>
2022-09-06 10:18:46 +0530

    new changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import Header from "./component/Header"
 import './App.css';
 
--- src/component/Header.js
+++ src/component/Header.js
@@ -1,9 +1,22 @@
+import './Header.css';
+
 const Header=(props)=>{
     return(
         <header>
-            <h1> Hello {props.title}</h1>
-            <h1> Hi {props.title1}</h1>
+            <h1 style={headingStyle}> Hello {props.title}</h1>
+            <h1 className = "h2Heading"> Hi {props.title1}</h1>
         </header>
     );
 };
+
+const headingStyle = {
+    color:"darkcyan",
+    backgroundColor:"lightblue",
+    textAlign:"center",
+};
+
+Header.defaultProps = {
+    title: "Task Tracker",
+};
+
 export default Header;
